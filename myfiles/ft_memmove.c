21702c5af2aeb258faf30bad60a4b98ebc770764
dpadovan <danpadovani84@gmail.com>
2021-05-27 19:36:14 -0300

    update

--- myfiles/ft_memmove.c
+++ myfiles/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: dpadovan <dpadovan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 22:52:53 by dpadovan          #+#    #+#             */
-/*   Updated: 2021/05/27 19:04:49 by dpadovan         ###   ########.fr       */
+/*   Updated: 2021/05/27 19:35:47 by dpadovan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	else
 	{
 		i = n - 1;
-		while (i && i < n)
+		while (i >= 0 && i < n)
 		{
 			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
 			i--;
