68684f1304203ff12125bd01a374e349531f9c38
dpadovan <danpadovani84@gmail.com>
2021-05-27 19:47:15 -0300

    Updating

--- myfiles/libft.h
+++ myfiles/libft.h
@@ -6,7 +6,7 @@
 /*   By: dpadovan <dpadovan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 20:23:29 by dpadovan          #+#    #+#             */
-/*   Updated: 2021/05/26 23:18:29 by dpadovan         ###   ########.fr       */
+/*   Updated: 2021/05/27 19:46:49 by dpadovan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,6 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+//char 	*strstr(const char *big, const char	*little);
 
 #endif
