cfaaea2d038bb0d315d20a7911d350d66c3e9586
dpadovan <danpadovani84@gmail.com>
2021-05-27 21:31:34 -0300

    update

--- myfiles/ft_strchr.c
+++ myfiles/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: dpadovan <dpadovan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 23:32:17 by dpadovan          #+#    #+#             */
-/*   Updated: 2021/05/27 20:06:13 by dpadovan         ###   ########.fr       */
+/*   Updated: 2021/05/27 21:18:52 by dpadovan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,9 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s != (char)c)
-	{
-		if (*s == '\0')
-			return (NULL);
-		s++;
-	}
-	return ((char *)s);
+	while (*s != '\0' && (char)c != *s)
+	s++;
+		if (*s == (char)c)
+			return ((char *)s);
+	return (NULL);
 }
--- myfiles/ft_strrchr.c
+++ myfiles/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: dpadovan <dpadovan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/22 01:06:33 by dpadovan          #+#    #+#             */
-/*   Updated: 2021/05/22 01:16:10 by dpadovan         ###   ########.fr       */
+/*   Updated: 2021/05/27 21:25:11 by dpadovan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,15 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int	i;
 	int	size;
+	
 
-	size = ft_strlen(s) + 1;
-	i = 0;
-	while (i < size)
-	{
-		i++;
-		s++;
-	}
-	while (size >= 0)
-	{
-		if (c == *s)
-			return ((char *)s);
-		s--;
+	size = ft_strlen(s);
+		
+	while (size >= 0 || c != s[size])
+	{	
+		if ((char)c == s[size])
+			return ((char *)s + size);
 		size--;
 	}
 	return (NULL);
