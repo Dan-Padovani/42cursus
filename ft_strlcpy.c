b64f1b722ae99085407858c7c65aee4c59316189
dpadovan <danpadovani84@gmail.com>
2021-05-29 23:56:36 -0300

    update

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: dpadovan <dpadovan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 21:20:03 by dpadovan          #+#    #+#             */
-/*   Updated: 2021/05/29 23:35:17 by dpadovan         ###   ########.fr       */
+/*   Updated: 2021/05/29 23:55:52 by dpadovan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,17 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
-	if (*dst == '\0' || *src == '\0')
-		return (0);	
-	if (size == 0)
-		return (ft_strlen(src));
-	i = 0;
-	while (i < size - 1 && src[i])
+	if (dst == NULL || src == NULL)
+		return (0);
+	if (size > 0)
 	{
-		dst[i] = src[i];
-		i++;
+		i = 0;
+		while (i < size - 1 && src[i])
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
 	}
-	dst[i] = '\0';
 	return (ft_strlen(src));
 }
